15e56f95f32d3d58f216594182358314f526456e
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2022-10-02 18:28:08 -0700

    Add project files.

---
+++ NoGameplayCursor/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NoGameplayCursor
+{
+    [HarmonyPatch]
+    [BepInPlugin("com.steven.trombone.nogameplaycursor", "No Gameplay Cursor", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        const string GAMEPLAY_SCENE_NAME = "gameplay";
+
+        void Awake()
+        {
+            var harmony = new Harmony("com.steven.trombone.nogameplaycursor");
+            harmony.PatchAll();
+
+            SceneManager.activeSceneChanged += ActiveSceneChanged;
+        }
+
+        void OnDestroy() => SceneManager.activeSceneChanged -= ActiveSceneChanged;
+
+        void ActiveSceneChanged(Scene _, Scene nextScene)
+        {
+            if (nextScene.name == GAMEPLAY_SCENE_NAME)
+                Cursor.visible = false;
+            else
+                Cursor.visible = true;
+        }
+
+        [HarmonyPatch(typeof(PauseCanvasController), nameof(PauseCanvasController.showPausePanel))]
+        static void Prefix() => Cursor.visible = true;
+    }
+}
